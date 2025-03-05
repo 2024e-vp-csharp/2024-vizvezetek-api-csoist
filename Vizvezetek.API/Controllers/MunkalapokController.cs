74fec8ac5baf0d974b8061eb343607878118178f
István Csóti <icsoti8@gmail.com>
2025-03-05 19:27:26 +0100

    MSQL connection + DTO + Controller

---
+++ Vizvezetek.API/Controllers/MunkalapokController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizvezetek.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunkalapokController : ControllerBase
+    {
+    }
+}
---
+++ Vizvezetek.API/DTOs/munkalapDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Vizvezetek.API.DTOs
+{
+    public class munkalapDTO
+    {
+        public munkalapDTO(int id, DateOnly beadasi_datum, DateOnly javitasi_datum, string telepules, string utca, string szerelo, int munkaora, int anyagar)
+        {
+            Id = id;
+            Beadasi_datum = beadasi_datum;
+            Javitasi_datum = javitasi_datum;
+            Helyszin = telepules + ", " + utca;
+            Szerelo = szerelo;
+            Munkaora = munkaora;
+            Anyagar = anyagar;
+        }
+
+        public int Id { get; set; }
+        public DateOnly Beadasi_datum { get; set; }
+        public DateOnly Javitasi_datum { get; set; }
+        public string Helyszin { get;set; }
+        public string Szerelo { get; set; }
+        public int Munkaora { get; set; }
+        public int Anyagar { get; set; }
+
+    }
+}
