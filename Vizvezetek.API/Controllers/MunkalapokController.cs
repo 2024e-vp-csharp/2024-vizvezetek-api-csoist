75312d40b404d71bc149229b5c6cd7db0b6c9c8e
István Csóti <icsoti8@gmail.com>
2025-03-06 18:38:29 +0100

    Conversion + GetMunkalap + GetMunkalapById

--- Vizvezetek.API/Controllers/MunkalapokController.cs
+++ Vizvezetek.API/Controllers/MunkalapokController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Vizvezetek.API.DTOs;
 using Vizvezetek.API.Models;
+using Vizvezetek.API.Conversion;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Vizvezetek.API.Controllers
 {
@@ -19,9 +22,19 @@ namespace Vizvezetek.API.Controllers
 
         // GET api/Munkalapok
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<munkalap>>> GetMunkalap()
+        public async Task<ActionResult<IEnumerable<munkalapDTO>>> GetMunkalap()
         {
-            return await _context.munkalap.ToListAsync();
+            var munkalapok = await _context.munkalap.Include(x => x.hely).Include(x => x.szerelo).ToListAsync();
+            return munkalapok.ToDTO();
         }
+
+        // GET api/Munkalapok/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<munkalapDTO>>> GetMunkalapById(int id)
+        {
+            var munkalapok = await _context.munkalap.Include(x => x.hely).Include(x => x.szerelo).Where(x => x.id == id).ToListAsync();
+            return munkalapok.ToDTO();
+        }
+
     }
 }
---
+++ Vizvezetek.API/Conversion/Conversion.cs
@@ -0,0 +1,18 @@
+﻿using Vizvezetek.API.DTOs;
+using Vizvezetek.API.Models;
+
+namespace Vizvezetek.API.Conversion
+{
+    public static class Conversion
+    {
+        public static munkalapDTO ToDTO(this munkalap munkalap)
+        {
+            return new munkalapDTO(munkalap.id, munkalap.beadas_datum, munkalap.javitas_datum, munkalap.hely.telepules, munkalap.hely.utca, munkalap.szerelo.nev, munkalap.munkaora, munkalap.anyagar);
+        }
+
+        public static List<munkalapDTO> ToDTO(this IEnumerable<munkalap> munkalap)
+        {
+            return munkalap.Select(ToDTO).ToList();
+        }
+    }
+}
